3c26d3d395b63d1dce0674db50bcdcbcf9d8706b
empaisley <empaisley@ucdavis.edu>
2013-10-27 08:30:12 -0700

    removed space in data path

--- HW1/BayesLogit/BLR_fit.R
+++ HW1/BayesLogit/BLR_fit.R
@@ -27,7 +27,7 @@ if (length(args)==0){
 }
 
 #data_path = "/home/paisley/Stuff/HW1/BayesLogit/data/ "
-data_path = "data/ "
+data_path = "data/"
 data = read.csv(paste0(data_path,"blr_data_",jobid, ".csv"))
 pars = read.csv(paste0(data_path,"blr_pars_",jobid, ".csv"))
                 
