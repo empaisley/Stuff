b66af65621c13d8536f635f6f69fcb67a3bfd988
empaisley <empaisley@ucdavis.edu>
2013-11-06 13:56:55 -0800

    adding mapper, reducer, and mini files to Gauss

--- HW2/Streaming/mapper.py
+++ HW2/Streaming/mapper.py
@@ -2,15 +2,42 @@ __author__ = 'EliotP'
 
 import sys
 
-# input comes from STDIN (standard input)
-
+file = open('out.txt', 'w')
 for line in sys.stdin:
+
     # remove leading and trailing whitespace
     line = line.strip()
-    # split the line into words
-    words = line.split()
-    #increase counters
-    for word in words:
-        #write results to STDOUT (standard output); what we output here will
-        # be the input for the Reduce step, i.e. the input for reducer .py
-        print '%s\t%s' % (word,1)
+    # split x and y values apart
+    line = line.split()
+
+    # x and y were split as a list of two elements.
+    # need to convert to floats and designate which element is x and which is y.
+    x = map(float,line)[0]
+    y = map(float,line)[1]
+
+    # create upper and lower bounds for each bin, separated by 0.1
+    x_lo = round(x,1)
+    x_hi = round(x,1)+0.1
+    y_lo = round(y,1)
+    y_hi = round(y,1)+0.1
+
+    #print((x_lo,x_hi,y_lo,y_hi),1)
+    #print '%s\t%s' % ((x_lo,x_hi,y_lo,y_hi),1)
+    file.write(x)
+
+file.close()
+
+#import sys
+#
+## input comes from STDIN (standard input)
+#
+#for line in sys.stdin:
+#    # remove leading and trailing whitespace
+#    line = line.strip()
+#    # split the line into words
+#    words = line.split()
+#    #increase counters
+#    for word in words:
+#        #write results to STDOUT (standard output); what we output here will
+#        # be the input for the Reduce step, i.e. the input for reducer .py
+#        print '%s\t%s' % (word,1)
---
+++ HW2/Streaming/reducer.py
@@ -0,0 +1,50 @@
+__author__ = 'EliotP'
+
+import csv
+
+ofile = csv.writer(open("TESTING.csv","wb"))
+writer = csv.writer(ofile, delimeter='\t')
+
+current_word = None
+current_count = 0
+word = None
+for line in sys.stdin:
+    line = line.strip()
+    word, count = line.split('\t',1)
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            # write result to STDOUT
+            writer.writerow(current_word, current_count)
+            #print '%s\t%s' % (current_word, current_count)
+        current_count = count
+        current_word = word
+if current_word == word:
+    writer.writerow(current_word, current_count)
+    #print '%s\t%s' % (current_word, current_count)
+
+#current_word = None
+#current_count = 0
+#word = None
+#for line in sys.stdin:
+#    line = line.strip()
+#    word, count = line.split('\t',1)
+#    try:
+#        count = int(count)
+#    except ValueError:
+#        continue
+#    if current_word == word:
+#        current_count += count
+#    else:
+#        if current_word:
+#            # write result to STDOUT
+#            print '%s\t%s' % (current_word, current_count)
+#        current_count = count
+#        current_word = word
+#if current_word == word:
+#    print '%s\t%s' % (current_word, current_count)
